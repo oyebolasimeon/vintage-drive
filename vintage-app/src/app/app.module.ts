673bbba61f2eb008199b681a74906ebf2fb110ab
simeon <simeonoyekunleoyebola@gmail.com>
2022-05-26 22:46:29 +0100

    Created all the needed components

--- vintage-app/src/app/app.module.ts
+++ vintage-app/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SigninComponent } from './Components/signin/signin.component';
+import { LogoutComponent } from './Components/logout/logout.component';
+import { HomeComponent } from './Components/dashboard/home/home.component';
+import { ClientComponent } from './Components/dashboard/client/client.component';
+import { VehicleComponent } from './Components/dashboard/vehicle/vehicle.component';
+import { ServicesComponent } from './Components/dashboard/services/services.component';
+import { QuoteComponent } from './Components/dashboard/quote/quote.component';
+import { InvoiceComponent } from './Components/dashboard/invoice/invoice.component';
+import { StaffComponent } from './Components/dashboard/staff/staff.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent,
+    LogoutComponent,
+    HomeComponent,
+    ClientComponent,
+    VehicleComponent,
+    ServicesComponent,
+    QuoteComponent,
+    InvoiceComponent,
+    StaffComponent
   ],
   imports: [
     BrowserModule,
