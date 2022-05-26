d18ab395d1ae99d50ffd9894663f0eb91d4138b1
simeon <simeonoyekunleoyebola@gmail.com>
2022-05-26 22:39:51 +0100

    boiler template

--- vintage-app/README.md
+++ vintage-app/README.md
@@ -1,4 +1,4 @@
-# VintageDriveApp
+# VintageApp
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.3.1.
 
--- vintage-app/karma.conf.js
+++ vintage-app/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/vintage-drive-app'),
+      dir: require('path').join(__dirname, './coverage/vintage-app'),
       subdir: '.',
       reporters: [
         { type: 'html' },
