efd21a0a099dd08539175fb8064aeb0e7d5dcb32
Daniel Abati <danielabatibabatunde1@gmail.com>
2022-05-27 08:48:15 +0100

    testfiles

--- vintage-app/src/app/app-routing.module.ts
+++ vintage-app/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SigninComponent } from './Components/signin/signin.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', component:SigninComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
