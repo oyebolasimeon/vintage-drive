102a54f466d4f720ac7ee13e5440321f8225e684
Godgoodness@14092001 <soyebola@atbtechsoft.com>
2022-05-28 08:26:04 +0100

    created dashboard routing modules

---
+++ vintage-app/src/app/Components/dashboard/dashboard-routing.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DashboardRoutingRoutingModule } from './dashboard-routing-routing.module';
+import { DashboardNavbarComponent } from './dashboard-navbar/dashboard-navbar.component';
+import { DashboardSidebarComponent } from './dashboard-sidebar/dashboard-sidebar.component';
+import { HomeComponent } from './home/home.component';
+import { InvoiceComponent } from './invoice/invoice.component';
+import { QuoteComponent } from './quote/quote.component';
+import { StaffComponent } from './staff/staff.component';
+import { ClientComponent } from './client/client.component';
+import { ServicesComponent } from './services/services.component';
+import { VehicleComponent } from './vehicle/vehicle.component';
+
+
+@NgModule({
+  declarations: [
+    DashboardNavbarComponent,
+    DashboardSidebarComponent,
+    HomeComponent,
+    InvoiceComponent,
+    QuoteComponent,
+    StaffComponent,
+    ClientComponent,
+    ServicesComponent,
+    VehicleComponent
+  ],
+  imports: [
+    CommonModule,
+    DashboardRoutingRoutingModule
+  ]
+})
+export class DashboardRoutingModule { }
