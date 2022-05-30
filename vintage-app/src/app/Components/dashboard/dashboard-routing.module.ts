05f08a454310e17cc0a423e2a5c36e03ce2ccd7a
Godgoodness@14092001 <soyebola@atbtechsoft.com>
2022-05-30 10:14:58 +0100

    dashboard routing

--- vintage-app/src/app/Components/dashboard/dashboard-routing.module.ts
+++ vintage-app/src/app/Components/dashboard/dashboard-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PagenotfoundComponent } from '../pagenotfound/pagenotfound.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent}
+  {path: '', component: HomeComponent, pathMatch: "full"},
+  {path: '**', component: PagenotfoundComponent}
 ];
 
 @NgModule({
