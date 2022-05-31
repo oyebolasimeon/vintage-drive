7dc19bb8aab58465c49d3f425db7f19907ed8052
Godgoodness@14092001 <soyebola@atbtechsoft.com>
2022-05-31 10:39:46 +0100

    signin, datatable and auth services included

--- vintage-app/src/app/Components/signin/signin.component.ts
+++ vintage-app/src/app/Components/signin/signin.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { faLock } from '@fortawesome/free-solid-svg-icons';
+import { AuthService } from 'src/app/service/auth.service';
 
 
 @Component({
@@ -11,11 +14,31 @@ export class SigninComponent implements OnInit {
 
     email = '';
     password = '';
+    responseData: any;
 
     falock = faLock;
-  constructor() { }
+  constructor( private service:AuthService, private router:Router) { 
+    localStorage.clear();
+  }
+
+  Login = new FormGroup({
+            email: new FormControl(" ", Validators.required),
+            password: new FormControl(" ", Validators.required)
+  })
 
   ngOnInit(): void {
+  }
+
+  ProceedLogin(){
+    if(this.Login.valid){
+      this.service.proceedLogin(this.Login.value).subscribe(results =>{
+        if(results != null){
+            this.responseData = results;
+            localStorage.setItem('token', this.responseData.payload.token);
+            this.router.navigate(['/admin'])
+        }
+      })
+    }
   }
 
 }
