4b9325720d9ab4033875c7326f295c41eb2eed5b
Godgoodness@14092001 <soyebola@atbtechsoft.com>
2022-06-08 10:49:25 +0100

    invoice logic update

--- vintage-app/src/app/Components/dashboard/invoice/invoice.component.ts
+++ vintage-app/src/app/Components/dashboard/invoice/invoice.component.ts
@@ -1,6 +1,5 @@
-import { TagDefinition } from '@angular/compiler';
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr'
 import { AuthService } from 'src/app/service/auth.service';
@@ -17,34 +16,57 @@ export class InvoiceComponent implements OnInit {
   postField = false;
   dataField = true;
   postBtnData = `Add New Invoice`
-  item= ''; totalAmount = ''; unit='';
+  item = ''; rates = ''; units = '';
   @ViewChild('code')
   code: 'td' = "td";
 
 
-  constructor(private service:AuthService, private toastr: ToastrService, private router: Router) {
-      this.service.GetInvoice().subscribe(result => {
-        this.invoiceList = result;
-        console.log(this.invoiceList.payload);
+  constructor(private fb: FormBuilder, private service: AuthService, private toastr: ToastrService, private router: Router) {
+    this.service.GetInvoice().subscribe(result => {
+      this.invoiceList = result;
+      console.log(this.invoiceList.payload);
 
-      } )
-   }
+    })
+  }
 
-   AddNewInvoice = new FormGroup({
-    item: new FormControl([], Validators.required),
-    unit: new FormControl(" ", Validators.required),
-    totalAmount: new FormControl(" ", Validators.required),
-   })
+  AddNewInvoice = this.fb.group({
+
+    billingAddress: this.fb.group({
+      repName: ['', Validators.required],
+      address: ['', Validators.required],
+      city: ['', Validators.required],
+      postalCode: ['', Validators.required],
+      state: ['', Validators.required]
+    }),
+
+    items: this.fb.group({
+      item: ['', Validators.required],
+      unit: ['', Validators.required],
+      rate: ['', Validators.required],
+      amount: [this.calculateAmt()]
+    }),
+
+    clientName: ['', Validators.required],
+    vehicleName: ['', Validators.required]
+
+  })
 
   ngOnInit(): void {
   }
 
-  showPostField(){
-    if(this.postField === false && this.dataField === true){
+  calculateAmt() {
+    const amt = this.AddNewInvoice.get('clientName').value
+    // * this.AddNewInvoice.value.items.unit
+    // console.log(amt)
+  
+  }
+
+  showPostField() {
+    if (this.postField === false && this.dataField === true) {
       this.postField = true
       this.dataField = false
       this.postBtnData = "Exit"
-    } else{
+    } else {
       this.postField = false
       this.dataField = true
       this.postBtnData = "Add New Invoice"
@@ -53,22 +75,25 @@ export class InvoiceComponent implements OnInit {
     return this.postField;
   }
 
-  addInvoice(){
+  onSubmit() {
+    console.warn(this.AddNewInvoice.value)
+  }
+  addInvoice() {
     let payload = this.AddNewInvoice.value
-      
-        
-      
-      
+
+
+
+
     console.log(payload)
-      this.service.AddNewInvoice(payload).subscribe(result => {
-        console.log(result);
-        this.toastr.success("New Invoice Added Successfully")
-      }, (error) => {
-        this.toastr.error(error.message)
-      })
+    this.service.AddNewInvoice(payload).subscribe(result => {
+      console.log(result);
+      this.toastr.success("New Invoice Added Successfully")
+    }, (error) => {
+      this.toastr.error(error.message)
+    })
   }
 
-  removeInvoice(code: any){
+  removeInvoice(code: any) {
     this.service.DeleteInvoice(code).subscribe(result => {
       console.log(result)
       this.toastr.info(`Invoice_ID ${code} got deleted`)
